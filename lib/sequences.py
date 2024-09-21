1339492e56fe9899d903ace1ae08eb6c785354ee
kacha930 <robiwere20@gmail.com>
2024-09-21 08:02:05 +0300

    fib sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+# Write a function print_fibonacci() that prints a list of the fibonacci sequenceLinks to an external site. up to the length provided in the function's parameters.
+
+# => [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+# fibonacci series - Each number is equal to the sum of the preceding two numbers. 
 
 def print_fibonacci(length):
-    pass
+    
+
+    fibonacci_sequence = [0,1]
+
+    if length <= 0:
+        print([])
+        return
+    
+    while len(fibonacci_sequence) < length:
+        next_fibonancci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_fibonancci)
+
+    print(fibonacci_sequence[:length])
+
+print_fibonacci(0)
